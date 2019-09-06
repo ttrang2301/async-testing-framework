66191b24c0e42a67948697330153a328230fcbed
TLe <TLe@absolute.com>
2019-09-06 17:07:27 +0700

    Add feature - Scan application source code for testing classes

---
+++ async-testing-framework-test/src/main/java/ttrang2301/asynctesting/anothertest/case1/AnotherTestCase1.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) 2019 Absolute Software Corporation. All rights reserved. Reproduction or
+ * transmission in whole or in part, in any form or by any means, electronic, mechanical or
+ * otherwise, is prohibited without the prior written consent of the copyright owner.
+ */
+package ttrang2301.asynctesting.anothertest.case1;
+
+import ttrang2301.asynctesting.AsyncTest;
+
+@AsyncTest(name = "TC-ABS1234-a1")
+public class AnotherTestCase1 {
+}
--- async-testing-framework-test/src/main/java/ttrang2301/asynctesting/test/TestingApplication.java
+++ async-testing-framework-test/src/main/java/ttrang2301/asynctesting/test/TestingApplication.java
@@ -3,8 +3,11 @@ package ttrang2301.asynctesting.test;
 
 import ttrang2301.asynctesting.AsyncTestingApplication;
 import ttrang2301.asynctesting.EnableAsyncTesting;
+import ttrang2301.asynctesting.othertests.case1.OtherTestCase1;
 
-@EnableAsyncTesting
+@EnableAsyncTesting(
+        basePackages = {"ttrang2301.asynctesting.anothertest"},
+        basePackageClasses = {OtherTestCase1.class})
 public class TestingApplication {
 
     public static void main(String[] args) {
