36121e82b8653c7e1b65e445778e2bc6721965f4
TLe <TLe@absolute.com>
2019-09-06 17:44:45 +0700

    Adjust feature - Scan application source code for precondition and expectation methods

--- async-testing-framework/src/main/java/ttrang2301/asynctesting/AsyncTestingApplication.java
+++ async-testing-framework/src/main/java/ttrang2301/asynctesting/AsyncTestingApplication.java
@@ -17,25 +17,34 @@ public class AsyncTestingApplication {
 
     public static void run(Class<?> mainClass, String[] args) {
         String primaryBasePackageName = mainClass.getPackage().getName();
-        log.info("Start testing application from package " + primaryBasePackageName);
-        EnableAsyncTesting annotation = mainClass.getAnnotation(EnableAsyncTesting.class);
-
-        // Collect should-be-scanned packages
-        List<String> scannedPackages = new ArrayList<>(Arrays.asList(annotation.basePackages()));
+        List<String> scannedPackages = new ArrayList<>();
         scannedPackages.add(primaryBasePackageName);
+        scannedPackages.addAll(extractTestingPackages(mainClass.getAnnotation(EnableAsyncTesting.class)));
+        Set<Class<?>> classes = extractTestingClasses(scannedPackages);
+        extractPreconditions(classes);
+        extractExpectations(classes);
+
+    }
+
+    private static List<String> extractTestingPackages(EnableAsyncTesting annotation) {
+        List<String> scannedPackages = new ArrayList<>(Arrays.asList(annotation.basePackages()));
         scannedPackages.addAll(
                 Arrays.stream(annotation.basePackageClasses())
                         .map(clazz -> clazz.getPackage().getName()).collect(Collectors.toList()));
+        return scannedPackages;
+    }
 
-        // Collect testing classes
+    private static Set<Class<?>> extractTestingClasses(List<String> scannedPackages) {
         Set<Set<Class<?>>> sets = scannedPackages.stream()
                 .map(scannedPackage -> new Reflections(scannedPackage).getTypesAnnotatedWith(AsyncTest.class))
                 .collect(Collectors.toSet());
         Set<Class<?>> classes = new HashSet<>();
         sets.forEach(classes::addAll);
         classes.forEach(testingClass -> log.info("=== Test class: {}", testingClass.getName()));
+        return classes;
+    }
 
-        // Collect preconditions
+    private static void extractPreconditions(Set<Class<?>> classes) {
         List<Method> preconditionMethods =
                 classes.stream()
                         .map(clazz -> Arrays.stream(clazz.getMethods())
@@ -44,8 +53,9 @@ public class AsyncTestingApplication {
                         .flatMap(List::stream)
                         .collect(Collectors.toList());
         preconditionMethods.forEach(method -> log.info("=== Precondition: {}", method.getName()));
+    }
 
-        // Collect expectations
+    private static void extractExpectations(Set<Class<?>> classes) {
         List<Method> expectationMethods =
                 classes.stream()
                         .map(clazz -> Arrays.stream(clazz.getMethods())
@@ -54,7 +64,6 @@ public class AsyncTestingApplication {
                         .flatMap(List::stream)
                         .collect(Collectors.toList());
         expectationMethods.forEach(method -> log.info("=== Expectation: {}", method.getName()));
-
     }
 
 
