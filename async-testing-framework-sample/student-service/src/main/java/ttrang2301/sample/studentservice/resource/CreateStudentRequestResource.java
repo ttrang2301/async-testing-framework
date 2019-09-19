0192e91cda7412eb546334e19777a62e8fdbe71e
Lê Thị Thùy Trang <ttrang2301@gmail.com>
2019-09-19 23:08:08 +0700

    Working on sample of asynchronous service

--- async-testing-framework-sample/student-service/src/main/java/ttrang2301/sample/studentservice/resource/CreateStudentRequestResource.java
+++ async-testing-framework-sample/student-service/src/main/java/ttrang2301/sample/studentservice/resource/CreateStudentRequestResource.java
@@ -1,22 +1,25 @@
 package ttrang2301.sample.studentservice.resource;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ttrang2301.sample.studentservice.model.CreateStudentForm;
 import ttrang2301.sample.studentservice.model.CreateStudentRequest;
-import ttrang2301.sample.studentservice.model.CreateStudentRequestStatus;
 import ttrang2301.sample.studentservice.model.Student;
 import ttrang2301.sample.studentservice.repository.CreateStudentRequestRepository;
 import ttrang2301.sample.studentservice.repository.StudentRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-@RestController("/create-student-request")
+@RestController
+@RequestMapping("/create-student-requests")
 public class CreateStudentRequestResource {
 
     @Autowired
@@ -26,16 +29,48 @@ public class CreateStudentRequestResource {
     private StudentRepository studentRepository;
 
     @PostMapping
-    public ResponseEntity<String> submitCreateStudentRequest(@RequestBody CreateStudentForm form) {
+    public ResponseEntity<String> createCreateStudentRequest(@RequestBody CreateStudentForm form) {
         String requestId = UUID.randomUUID().toString();
+        CreateStudentRequest request = new CreateStudentRequest(requestId, form, 0, null);
+        requestRepository.save(new ttrang2301.sample.studentservice.model.CreateStudentRequest(requestId, form.getStudentName(), 0, null));
         ExecutorService executor = Executors.newSingleThreadExecutor();
-        executor.submit(new CreateStudentRunnable(requestRepository, studentRepository, form));
+        executor.submit(new CreateStudentRunnable(requestRepository, studentRepository, request));
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(requestId);
     }
 
     @GetMapping("/{id}")
-    public CreateStudentRequest getCreateStudentRequest(@PathVariable String id) {
-        return requestRepository.getOne(id);
+    public ResponseEntity<CreateStudentRequest> getCreateStudentRequest(
+            @PathVariable String id) {
+        Optional<ttrang2301.sample.studentservice.model.CreateStudentRequest> request = requestRepository.findById(id);
+        if (!request.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(new CreateStudentRequest(
+                request.get().getId(),
+                new CreateStudentForm(request.get().getForm().getStudentName()),
+                request.get().getStatus(),
+                request.get().getCreatedStudentId()));
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    private static final class CreateStudentRequest {
+
+        private String id;
+        private CreateStudentRequestResource.CreateStudentForm form;
+        private int status;
+        private String createStudentId;
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    private static final class CreateStudentForm {
+
+        private String studentName;
+
     }
 
     private static final class CreateStudentRunnable implements Runnable {
@@ -43,30 +78,39 @@ public class CreateStudentRequestResource {
         private CreateStudentRequestRepository requestRepository;
         private StudentRepository studentRepository;
 
-        private CreateStudentForm form;
+        private CreateStudentRequestResource.CreateStudentRequest request;
 
         private CreateStudentRunnable(CreateStudentRequestRepository requestRepository,
                                       StudentRepository studentRepository,
-                                      CreateStudentForm form) {
+                                      CreateStudentRequestResource.CreateStudentRequest request) {
             this.requestRepository = requestRepository;
             this.studentRepository = studentRepository;
-            this.form = form;
+            this.request = request;
         }
 
         @Override
         public void run() {
-            CreateStudentRequest request = new CreateStudentRequest(this.form.getStudentName());
-            request = requestRepository.save(request);
-            for (int i=0; i<10; i++) {
+            String createdStudentId = UUID.randomUUID().toString();
+            System.out.println("START processing request " + request.getId() + " - Creating student " + createdStudentId);
+            ttrang2301.sample.studentservice.model.CreateStudentRequest persistedRequest = requestRepository.findById(request.getId()).get();
+            for (int i=0; i<9; i++) {
                 try {
-                    TimeUnit.SECONDS.sleep(1);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
+                    System.out.println("Encounter issue ");
                     e.printStackTrace();
                 }
-                request.setStatus((i + 1) * 10);
-                requestRepository.save(request);
+                int progress = (i + 1) * 10;
+                persistedRequest.setStatus(progress);
+                persistedRequest = requestRepository.save(persistedRequest);
+                System.out.println("PROCESSING request " + request.getId() + " - Creating student " + createdStudentId + ": " + progress + "%");
             }
-            studentRepository.save(new Student(request.getCreateStudentId(), request.getStudentName()));
+            Student student = new Student(createdStudentId, request.getForm().getStudentName());
+            studentRepository.save(student);
+            persistedRequest.setCreatedStudentId(createdStudentId);
+            persistedRequest.setStatus(100);
+            requestRepository.save(persistedRequest);
+            System.out.println("COMPLETE request " + request.getId() + " - Creating student " + createdStudentId);
         }
     }
 
--- async-testing-framework-sample/student-service/src/main/java/ttrang2301/sample/studentservice/resource/StudentResource.java
+++ async-testing-framework-sample/student-service/src/main/java/ttrang2301/sample/studentservice/resource/StudentResource.java
@@ -1,17 +1,40 @@
 package ttrang2301.sample.studentservice.resource;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ttrang2301.sample.studentservice.model.Student;
+import ttrang2301.sample.studentservice.repository.StudentRepository;
 
-@RestController("/students")
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/students")
 public class StudentResource {
 
-    @GetMapping
-    public Student getStudentByName(@RequestParam String name) {
-        // TODO
-        return null;
+    @Autowired
+    private StudentRepository repository;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudent(@PathVariable String id) {
+        Optional<ttrang2301.sample.studentservice.model.Student> student = repository.findById(id);
+        return student.isPresent()
+                ? ResponseEntity.status(HttpStatus.OK).body(new Student(student.get().getId(), student.get().getName()))
+                : ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    private static final class Student {
+
+        private String id;
+        private String name;
     }
 
 }
