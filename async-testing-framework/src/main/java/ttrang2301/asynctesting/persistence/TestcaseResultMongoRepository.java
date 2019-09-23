45f3911fa5ecb4e50a7dfe18538ba1d942dc94b2
Lê Thị Thùy Trang <ttrang2301@gmail.com>
2019-09-23 18:02:18 +0700

    Fix

--- async-testing-framework/src/main/java/ttrang2301/asynctesting/persistence/TestcaseResultMongoRepository.java
+++ async-testing-framework/src/main/java/ttrang2301/asynctesting/persistence/TestcaseResultMongoRepository.java
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) 2019 Absolute Software Corporation. All rights reserved. Reproduction or
- * transmission in whole or in part, in any form or by any means, electronic, mechanical or
- * otherwise, is prohibited without the prior written consent of the copyright owner.
- */
 package ttrang2301.asynctesting.persistence;
 
 import java.util.List;
