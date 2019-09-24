4fbcd629444c916f4298a5e47fa2e9d3c40bc7ef
Lê Thị Thùy Trang <ttrang2301@gmail.com>
2019-09-24 21:11:02 +0700

    Working on feature - Save Test Result to MongoDB

---
+++ async-testing-framework/src/test/java/ttrang2301/asynctesting/persistence/TestcaseResultMongoRepositoryTest.java
@@ -0,0 +1,14 @@
+package ttrang2301.asynctesting.persistence;
+
+import org.junit.Test;
+
+public class TestcaseResultMongoRepositoryTest {
+
+    // TODO optimize unit test
+    @Test
+    public void test() {
+        new TestcaseResultMongoRepository().updateExpectationStatus(
+                "campaign 2", "test 3", "expect3", TestcaseResult.Expectation.Status.UNKNOWN);
+    }
+
+}
