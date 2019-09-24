4781a2c60b8695de164bbbe13c9b1bbbfbd6fdeb
Lê Thị Thùy Trang <ttrang2301@gmail.com>
2019-09-24 22:34:37 +0700

    Add sample of a testing application

--- async-testing-framework-sample/student-service-test/src/main/java/ttrang2301/asynctesting/sample/case1/TestCase1.java
+++ async-testing-framework-sample/student-service-test/src/main/java/ttrang2301/asynctesting/sample/case1/TestCase1.java
@@ -1,28 +1,59 @@
 package ttrang2301.asynctesting.sample.case1;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import ttrang2301.asynctesting.annotation.AsyncTest;
 import ttrang2301.asynctesting.annotation.Expectation;
 import ttrang2301.asynctesting.annotation.Precondition;
 
-@AsyncTest(name = "TC-ABS1234")
+@AsyncTest(name = "TC-ABC1234")
 public class TestCase1 {
 
-    // Executed -> Waiting -> Done
-    // Executed | Waiting | Done | Timeout
-
     @Precondition
-    public void createCreateStudentRequest3() {
-        // TODO
-        // sendCreateStudentRequest(new CreateStudentRequest("A", "Male"));
+    public void sumitCreatStudentForm() {
+        // TODO sumitCreatStudentForm
         System.out.println("Public Preconsitions initialized");
     }
 
-    @Expectation(key = "", eventName = "")
-    public void assertStudentCreatedEvent(Object e) {
-//  public void assertStudentCreatedEvent(StudentCreatedEvent eventName) {
-        // assertThat(eventName.student.name == "A");
-        // assertThat(eventName.student.gender == "Male");
-        // studentRepository.get();
+    @Expectation(key = "form.accepted", eventName = "CreateStudentRequestCreated")
+    public void assertCreateStudentRequestCreatedEvent(CreateStudentRequestCreatedEvent event) {
+        // TODO assertCreateStudentRequestCreatedEvent
+    }
+
+    @Expectation(key = "student.created", eventName = "StudentCreated")
+    public void assertStudentCreatedEvent(StudentCreatedEvent event) {
+        // TODO assertStudentCreatedEvent
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static final class CreateStudentRequestCreatedEvent {
+
+        private String id;
+        private CreateStudentForm form;
+        private int status;
+        private String createStudentId;
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static final class CreateStudentForm {
+
+        private String studentName;
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static final class StudentCreatedEvent {
+
+        private String id;
+        private String name;
     }
 
 }
--- async-testing-framework-sample/student-service/src/main/java/ttrang2301/sample/studentservice/resource/StudentResource.java
+++ async-testing-framework-sample/student-service/src/main/java/ttrang2301/sample/studentservice/resource/StudentResource.java
@@ -79,7 +79,6 @@ public class StudentResource {
         }
     }
 
-
     @Data
     @AllArgsConstructor
     @NoArgsConstructor
